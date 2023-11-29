9ba700b68d2e3fa3552a884c616c39ccfc2cac07
Ulvi Asadov <148266987+ulviasadovv@users.noreply.github.com>
2023-11-30 03:37:46 +0400

    Add files via upload

---
+++ GenericList.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericList
+{
+    class GenericList<T>
+    {
+        private List<T> items;
+        private int CapacityInc;
+
+        public GenericList(int currentCap = 4, int capacityInc = 0)
+        {
+            items = new List<T>(currentCap);
+            CapacityInc = capacityInc;
+        }
+
+        public int Count 
+        { 
+            get { return items.Count; }
+        }
+
+        public int Capacity 
+        { 
+            get { return CapacityInc; } 
+        }
+
+
+
+        public void Add(T item)
+        {
+            items.Add(item);
+            if(Count == CapacityInc)
+            {
+                items.Capacity += CapacityInc;
+            }
+        }
+
+
+
+        public T Find(Predicate<T> match)
+        {
+            return items.Find(match);
+        }
+
+
+        public List<T> FindAll(Predicate<T> match)
+        {
+            return items.FindAll(match);
+        }
+
+
+
+        public bool Contains(T item)
+        {
+            return items.Contains(item);
+        }
+
+
+
+        public bool Exists(Predicate<T> match)
+        {
+            return items.Exists(match);
+        }
+
+
+
+        public bool Remove(T item)
+        {
+            bool removed = items.Remove(item);
+
+            if(removed && Count + CapacityInc < Capacity)
+            {
+                items.Capacity -= CapacityInc;
+            }
+
+            return removed;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿static void Main()
+{
+    CustomGenericList<int> customList = new CustomGenericList<int>(capacityInk: 8);
+
+
+    CustomGenericList.Add(1);
+    CustomGenericList.Add(2);
+    CustomGenericList.Add(3);
+}
